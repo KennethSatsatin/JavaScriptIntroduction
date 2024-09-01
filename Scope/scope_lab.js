2e113ad160d6e11fce48fb1b7801d744752a5e5e
KennethSatsatin <kennethsatsatin12@gmail.com>
2024-09-01 02:26:53 -0400

    curly bracket

--- Scope/scope_lab.js
+++ Scope/scope_lab.js
@@ -27,3 +27,19 @@ show();
 //console.log(functionVar);
 //console.log(functionLet);
 //console.log(functionConst);
+
+{
+    let blockLet = 'Inside block with let';
+    const blockConst = 'Inside block with const';
+    var blockVar = 'Inside block with var';
+
+    // Attempting to reassign variables within the block
+    blockLet = 'Reassigned inside block';  // This is allowed
+    // blockConst = 'Reassigned inside block';  // This will cause an error
+    blockVar = 'Reassigned inside block';  // This is allowed
+}
+
+console.log(blockLet);  // This will cause an error because blockLet is not defined outside the block
+console.log(blockConst);  // This will cause an error because blockConst is not defined outside the block
+console.log(blockVar);  
+blockVar = 'Reassigned outside block'; 

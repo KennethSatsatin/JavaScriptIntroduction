7530a1efcc3b2d946c70992a9b5f7ba6fb8c0db9
KennethSatsatin <kennethsatsatin12@gmail.com>
2024-09-01 04:58:28 -0400

    practice task

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -54,3 +54,22 @@ let isAuthenticated = true;
 let authenticationStatus = isAuthenticated ? "Authenticated" : "Not authenticated";
 
 console.log("Authentication Status:", authenticationStatus);
+
+let checkAccess = prompt("What is your role?");
+let role;
+
+switch (checkAccess) {
+    case "Employee":
+        role = "You are authorized to access 'Dietary Services'.";
+        break;
+    case "Enrolled Member":
+        role = "You are authorized to access 'Dietary Services' and have one-on-one interaction with a dietician.";
+        break;
+    case "Subscriber":
+        role = "You are authorized to have partial access to 'Dietary Services'.";
+        break;
+    default:
+        role ="You need to enroll first to avail the facility.";    
+}
+
+console.log(role);

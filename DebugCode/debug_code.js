528cb8274f7b21345baa131d4025c95948846211
KennethSatsatin <kennethsatsatin12@gmail.com>
2024-09-03 04:12:48 -0400

    practice task

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -6,25 +6,37 @@ function performOperation(){
     //Check if inputs are valid numbers
     if (!isNaN(num1) && !isNaN(num2)){
         //Perform the operation
-        let result = multiply(num1, num2);
+        let result = arithmeticOperation(num1, num2);
 
         //display the result
         displayResult(result);
     } else {
-        displayResult('Please enter valid numbers');
+        displayResult({
+            sum: 'Invalid input',
+            difference: 'Invalid iput',
+            product: 'Invalid input',
+            quotient: 'Invalid input'
+        });
     }
 
-    function multiply(a,b){
+    function arithmeticOperation(a,b){
         //Introduce a debugger statement to pause execution
         debugger;
-
+        let sum = a + b;
+        let difference = a - b;
+        let product = a * b;
+        let quotient =  a/b;
         //Multiply the numbers
-        return a * b;
+        return {sum, difference, product, quotient};
     }
 
     function displayResult(result){
         //Display the result in the paragraph element
         const resultElement = document.getElementById('result');
-        resultElement.textContent = `The result is: ${result}`;
+        resultElement.innerHTML = `
+        <p>The sum is: ${result.sum}</p>
+        <p>The Difference is: ${result.difference}</p>
+        <p>The Product is: ${result.product}</p>
+        <p>The Quotient is: ${result.quotient}</p>`;
     }
 }

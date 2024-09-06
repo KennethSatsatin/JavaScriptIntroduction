35cc49d40606f77c6cac65d58bf0b7ada16fb3be
KennethSatsatin <kennethsatsatin12@gmail.com>
2024-09-06 11:21:49 -0400

    practice task

--- weatherReport/weather_report.js
+++ weatherReport/weather_report.js
@@ -1,9 +1,12 @@
 function showweatherDetails(event) {
     event.preventDefault();
 
-    const city = document.getElementById('city').value;
+    //const city = document.getElementById('city').value;
+    const lat = document.getElementById('lat').value;
+    const lon = document.getElementById('lon').value;
     const apiKey = '98e88707a19baaf8e7ae72fa6e452339';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    //const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
     fetch(apiUrl)
         .then(response => response.json())
